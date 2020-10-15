d22e7d5f95e55c1f439e85e84adfbb93c5fb8f4b
gabrielppd77 <gabrielppd77@outlook.com>
2020-10-14 21:24:22 -0300

    Enviando o app

--- src/main/java/com/web/codeblog/controller/CodeblogController.java
+++ src/main/java/com/web/codeblog/controller/CodeblogController.java
@@ -1,27 +1,65 @@
 package com.web.codeblog.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 import com.web.codeblog.model.Post;
 import com.web.codeblog.service.CodeblogService;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class CodeblogController {
 
 	@Autowired
 	CodeblogService codeblogService;
-	
-	@RequestMapping(value = "/posts", method = RequestMethod.GET)
-	public ModelAndView getPosts() {
-		ModelAndView mv = new ModelAndView("posts");
+
+	@RequestMapping(value = "/", method = RequestMethod.GET) //basicamente é o link que o usuario acessa
+	public ModelAndView getPostsHome() { // Metodo adicionado para quando o "/" for acessado mostrar os POSTS
+		ModelAndView mv = new ModelAndView("posts"); //PAGINA HTML POSTS
 		List<Post> posts = codeblogService.findAll();
 		mv.addObject("posts", posts);
 		return mv;
 	}
+	
+	@RequestMapping(value = "/posts", method = RequestMethod.GET) //basicamente é o link que o usuario acessa
+	public ModelAndView getPosts() {
+		ModelAndView mv = new ModelAndView("posts"); //PAGINA HTML POSTS
+		List<Post> posts = codeblogService.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+	}
+
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails"); //OUTRA PAGINA HTML, Isso insere a pagina
+		Post post = codeblogService.findById(id); // pegando os posts do service
+		mv.addObject("post", post);
+		return mv;
+	}
+
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm(){ //acessar o formulario para cadastrar um post
+		return "postForm";
+	}
+
+	@RequestMapping(value="/newpost", method=RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes){
+        if(result.hasErrors()){
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        codeblogService.save(post);
+        return "redirect:/posts";
+    }
 }
--- src/main/java/com/web/codeblog/model/Post.java
+++ src/main/java/com/web/codeblog/model/Post.java
@@ -1,69 +1,69 @@
 package com.web.codeblog.model;
 
-import java.time.LocalDate;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.Table;
-
-import org.springframework.lang.NonNull;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
 
 @Entity
 @Table(name="TB_POST")
 public class Post {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	
-	@NonNull //Não aceitar NULL nesse valor
-	private String titulo;
-	
-	@NonNull
-	private String autor;
-	
-	@JsonFormat(shape= JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy")
-	private LocalDate data;
-	
-	@NonNull
-	@Lob //Avisar o banco que esta variavel terá valores grandes
-	private String texto;
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getAutor() {
-		return autor;
-	}
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	public LocalDate getData() {
-		return data;
-	}
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
-	public String getTexto() {
-		return texto;
-	}
-	public void setTexto(String texto) {
-		this.texto = texto;
-	}
-	
-	
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank // nao permite ficar vazio ou nulo
+    private String titulo;
+
+    @NotBlank
+    private String autor;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy") //formata a data
+    private LocalDate data;
+
+    @NotBlank
+    @Lob // avisa que o texto será longo
+    private String texto;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
